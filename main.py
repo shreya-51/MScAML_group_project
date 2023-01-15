47d3ce29ff84119836e486525e6ad81fc01fa92e
Shreya Wadehra <shreya.cube@gmail.com>
2023-01-15 12:08:40 +0000

    Updated incorrect comment

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import utils.freq_sweep as freq_sweep
 sweep_time    = 3      # frequency sweep lasts 3 seconds
 min_freq      = 20     # 20Hz
 max_freq      = 20000  # 20kHz
-sampling_rate = 100000 # 10kHz
+sampling_rate = 100000 # 100kHz
 
 def main():
     freq_sweep.plot_all_sweeps(sweep_time, sampling_rate, min_freq, max_freq)
