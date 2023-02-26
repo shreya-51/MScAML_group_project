8f73198d00f00958e65a044c9e00af83476c662a
lukemkU <lucas.iij27@gmail.com>
2023-02-26 16:18:47 +0000

    Changed chirp duration

--- c_code/sweep.c
+++ c_code/sweep.c
@@ -1,17 +1,20 @@
 #include <Arduino.h>
 
 int speakerPin = 9;
-int frequency = 20;
+int min_freq = 20;
+int max_freq = 20000;
 int sweepDuration = 5000; // 5 seconds
 int increment = 5;
+
+int num_freq = (max_freq - min_freq) / increment;
 
 void setup() {
   pinMode(speakerPin, OUTPUT);
 }
 
 void loop() {
-  for (frequency = 20; frequency <= 20000; frequency += increment) {
-    tone(speakerPin, frequency, sweepDuration/increment);
-    delay(sweepDuration/increment);
+  for (frequency = min_freq; frequency <= max_freq; frequency += increment) {
+    tone(speakerPin, frequency, sweepDuration/num_freq);
+    delay(sweepDuration/num_freq);
   }
 }
